819160d656a582b5040fc3b32061bdbe5d23444f
JrSntt <evandrojunior1615@gmail.com>
2020-02-20 20:13:08 -0300

    Layout form canva home

--- src/app/sign-up/sign-up.page.ts
+++ src/app/sign-up/sign-up.page.ts
@@ -1,3 +1,4 @@
+import { PouchDBUserService } from './../../../services/pouch-dbuser.service';
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
@@ -7,15 +8,40 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./sign-up.page.scss'],
 })
 export class SignUpPage implements OnInit {
+  user: any = {
+    id: '',
+    username: '',
+    phone: '',
+    email: '',
+    password: ''
+  }
 
   constructor(
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    private pouchDBUser: PouchDBUserService
   ) { }
 
   ngOnInit() {
   }
   openSignIn(){
     this.navCtrl.navigateRoot("sign-in");
+  }
+  sendUser(user){
+    user._id = Math.random().toString(36).substr(2, 9);
+
+    this.pouchDBUser.insertUserPouchDB(user).then(() => {
+      this.user = {
+        id: '',
+        username: '',
+        phone: '',
+        email: '',
+        password: ''
+      }
+      this.ngOnInit();
+      this.navCtrl.navigateRoot("home");
+    }).cath((err) => {
+      console.log('An error has occurred', err);
+    })
   }
 
 }
